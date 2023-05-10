b4a8bc6bbee69653d1b61cde5147f0ea4c687e07
ozzy59 <osergio@interia.pl>
2023-05-10 12:57:37 +0200

    dupa

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,14 @@ resource "aws_security_group" "ssh" {
   name = var.sg_ssh_name
 
   ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description      = "SSH"
     from_port        = 22
     to_port          = 22
